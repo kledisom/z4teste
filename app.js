184884d9d42e1176c757672ebf9ba6352288c7b4
kledisom <60370756+kledisom@users.noreply.github.com>
2023-05-09 12:50:43 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
-const port = process.env.PORT || 3033;
+const port = process.env.PORT || 3002;
 
 app.use(express.json());
 
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 });
 
-app.post("/card", async (req, res) => {
+app.post("/card", (req, res) => {
     var myHeaders = {
         "Content-Type": "application/json",
         "Client-Code": "FC-SB-15",
